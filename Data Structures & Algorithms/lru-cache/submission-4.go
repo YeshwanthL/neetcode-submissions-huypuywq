7382d644d18f019960612cadc342ce9988efa592
Yeshwanth <52047085+YeshwanthL@users.noreply.github.com>
2026-06-13 11:07:01 +0530

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.go
@@ -0,0 +1,65 @@
+type Node struct{
+    key int
+    val int
+    next *Node
+    prev *Node
+}
+
+type LRUCache struct {
+    size int
+    cache map[int]*Node
+    left, right *Node    
+}
+
+func Constructor(capacity int) LRUCache {
+    lru := LRUCache{
+        size: capacity,
+        cache: make(map[int]*Node),
+        left: &Node{},
+        right: &Node{},
+    }
+    lru.left.next = lru.right
+    lru.right.prev = lru.left
+    return lru
+}
+
+func (this *LRUCache) insert(node *Node){
+    prev, next := this.right.prev, this.right
+    prev.next = node
+    next.prev = node
+    node.prev = prev
+    node.next = next
+}
+
+func (this *LRUCache) remove(node *Node){
+    prev, next := node.prev, node.next
+    prev.next = next
+    next.prev = prev
+}
+
+func (this *LRUCache) Get(key int) int {
+    if node, ok := this.cache[key]; ok{
+        this.remove(node)
+        this.insert(node)
+        return node.val
+    }
+    return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+    if node,ok := this.cache[key]; ok{
+        this.remove(node)
+        delete(this.cache, key)
+    }
+
+    node := &Node{key: key, val:value}
+    this.cache[key] = node
+    this.insert(node)
+
+    if len(this.cache) > this.size{
+        lru := this.left.next
+        this.remove(lru)
+        delete(this.cache, lru.key)
+    }
+    
+}
