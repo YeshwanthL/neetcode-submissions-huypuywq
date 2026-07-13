18507c382fe5d3c5479ff48cc11f1bc928266aad
Yeshwanth <52047085+YeshwanthL@users.noreply.github.com>
2026-07-13 20:19:12 +0530

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        path = set()
+        def dfs(i ,j):
+            if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] == 0:
+                return 1
+            if (i,j) in path:
+                return 0
+            
+            path.add((i, j))
+            perimeter = dfs(i,j+1) + dfs(i, j-1) + dfs(i+1, j) + dfs(i-1, j)
+            return perimeter
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]:
+                    return dfs(i, j)
+        return 0
